fdcf955b543bfe7b29b37f4946a8ba263bbf5575
Mathieu Adimi <Mathieu.Adimi@chem.loc>
2025-06-16 22:36:31 +0200

    ajout de la fonctionnalité sur les articles publics

---
+++ larablog/app/Http/Controllers/PublicController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Article;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class PublicController extends Controller
+{
+    public function index(User $user)
+    {
+        // On récupère les articles publiés de l'utilisateur
+        $articles = Article::where('user_id', $user->id)->where('draft', 0)->get();
+
+        // On retourne la vue
+        return view('public.index', [
+            'articles' => $articles,
+            'user' => $user
+        ]);
+    }
+    public function show(User $user, Article $article)
+    {
+        // $user est l'utilisateur de l'article
+        // $article est l'article à afficher
+
+        // Je vous laisse faire le code
+        // N'oubliez pas de vérifier que l'article est publié (draft == 0)
+         // Vérifie que l'article appartient bien à l'utilisateur passé dans l'URL
+    if ($article->user_id !== $user->id) {
+        abort(403); // pour dire "accès refusé"
+    }
+ 
+    // Vérifie que l'article est publié
+    if ($article->draft) {
+        abort(403, 'Article en brouillon.');
+    }
+ 
+    // Affiche l'article dans la vue show
+    return view('public.show', [
+        'article' => $article,
+        'user' => $user
+    ]);
+    }
+}
--- larablog/routes/web.php
+++ larablog/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PublicController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -22,5 +23,6 @@ Route::middleware('auth')->group(function () {
     Route::post('/articles/{article}/update', [UserController::class, 'update'])->name('articles.update');
     Route::get('/articles/{article}/remove', [UserController::class, 'remove'])->name('articles.remove');
 });
-
+Route::get('/{user}', [PublicController::class, 'index'])->name('public.index');
+Route::get('/{user}/{article}', [PublicController::class, 'show'])->name('public.show');
 require __DIR__ . '/auth.php';
